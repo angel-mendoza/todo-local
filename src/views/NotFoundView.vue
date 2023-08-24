45a63d6c310925a1d5c4c32101c913c37450c0df
Criss.dev <criss.dev.01@gmail.com>
2023-08-24 09:27:27 -0500

    404 page

--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -3,8 +3,8 @@
     <div class="card">
       <div class="card-body text-center">
         <h3 class="card-title">404</h3>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
+        <p class="card-text">An error occurred, please try again later..</p>
+        <router-link class="btn btn-primary" to="/">Go Back</router-link>
       </div>
     </div>
   </div>
